cb0983b6fcf13e98e840e33f4e34e6cc413b211d
PeterDaish <965261@swansea.ac.uk>
2018-11-18 18:47:27 +0000

    Added constructor with "genre" and "languages"

--- src/resources/Book.java
+++ src/resources/Book.java
@@ -131,6 +131,46 @@ public class Book extends Resource {
 		this.genre = genre;
 		this.publisher = publisher;
 		this.isbn = isbn;
-		// isbn and languages are left blank due to being optional extras.
+		//languages is left blank due to being optional extra.
+	}
+
+	/**
+	 * This constructor for Book including genre and languages attributes.
+	 * @param author
+	 * The author of the book.
+	 * @param genre
+	 * The genre of the book.
+	 * @param lang
+	 * The languages this book is currently available in.
+	 * @param publisher
+	 * The publisher of the book.
+	 * @param year
+	 * The year the book was published.
+	 * @param title
+	 * The title of the book.
+	 * @param thumbnailImg
+	 * The filepath of the book's avatar.
+	 * @param uniqueID
+	 * The unique id of the book.
+	 * @param dictionaryOfCopies
+	 * To keep track of copies of this book.
+	 * @param queueOfReservations
+	 * To keep track of users who have reserved this book.
+	 * @param borrowHistory
+	 * The borrow history of each copy of this book.
+	 */
+	public Book(String author, String genre, ArrayList<String> lang, String publisher, String year, String title, String thumbnailImg,
+			String uniqueID, Dictionary dictionaryOfCopies, 
+			Queue queueOfReservations, Dictionary borrowHistory) {
+		
+		//Set all the inherited values from Resource...
+		super(year, title, thumbnailImg, uniqueID, dictionaryOfCopies, queueOfReservations, borrowHistory);
+		
+		//Set all non-inherited values
+		this.author = author;
+		this.genre = genre;
+		this.publisher = publisher;
+		this.languages = lang;
+		//isbn is left blank due to being optional extra.
 	}
 }
