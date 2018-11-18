030f96eeaa9bfd621e9767a60a088cd24d2065f9
PeterDaish <965261@swansea.ac.uk>
2018-11-18 16:11:07 +0000

    Added Book Class

--- src/resources/Book.java
+++ src/resources/Book.java
@@ -1,8 +1,8 @@
 package resources;
 /**
  * This class models a Library book.
- * @author Peter
- *
+ * @author Peter Daish
+ * @since 18/11/2018
  */
 public class Book extends Resource {
 	//add constructors...
