66424fa7f5447e093d3d4e43c1570be8ed711f3a
PeterDaish <965261@swansea.ac.uk>
2018-11-19 23:35:03 +0000

    Book Test Class

---
+++ src/resources/BookTest.java
@@ -0,0 +1,31 @@
+package resources;
+/**
+ * This class is solely to test Book
+ * @author PeterDaish
+ * @since 19/11/2018
+ */
+public class BookTest {
+/*
+	public static void main(String[] args) {
+		//Test Book Function.
+		Book lordOfRings = new Book("1943", "The Lord Of The Rings: The Fellowship of the ring",
+									"path", "00-00-00", "J.R.R Tolkein", "History", "124454", "Tolkein Estate", null );
+		
+		//Print out book created
+		System.out.println(lordOfRings.toString());
+		
+		System.out.println("\n\n");
+		//Add a new language to book
+		lordOfRings.addLanguage("English");
+		
+		System.out.println(lordOfRings.toString());
+		//add another new language
+		lordOfRings.addLanguage("German");
+		
+		System.out.println(lordOfRings.toString());
+		//delete language
+		lordOfRings.removeLanguage("English");
+		System.out.println(lordOfRings.toString());
+	}
+ */
+}
