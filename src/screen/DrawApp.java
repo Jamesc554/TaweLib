1c63cd62d4272bf0f11302fb4293107ae42b6c2e
James Carter <jamesc554544@gmail.com>
2018-11-19 15:30:40 +0000

    Updated DrawApp, changed JSON filepath to go to a data folder

--- src/screen/DrawApp.java
+++ src/screen/DrawApp.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
 
 public class DrawApp extends Screen {
 
@@ -12,20 +15,30 @@ public class DrawApp extends Screen {
 	public void start() {
 		components = new ArrayList<>();
 
-		Canvas canvas = new Canvas(1280, 720);
+		Canvas canvas = new Canvas(640, 380);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
+		GridPane grid = new GridPane();
+		
+		ToggleGroup tools = new ToggleGroup();
+		RadioButton paintBrushBtn = new RadioButton("Paint Brush");
+		paintBrushBtn.setToggleGroup(tools);
+		paintBrushBtn.setSelected(true);
+		
+		RadioButton paintBucketBtn = new RadioButton("Paint Bucket");
+		paintBucketBtn.setToggleGroup(tools);
+		
+		ColorPicker cPicker = new ColorPicker();
+		
 
 		gc.setLineWidth(5);
-		gc.setFill(Color.BLACK);
 
 		canvas.setOnMousePressed(mouse -> {
+			gc.setStroke(cPicker.getValue());
 			gc.beginPath();
 			gc.lineTo(mouse.getX(), mouse.getY());
 		});
 
-		canvas.setOnMouseDragged(mouse ->
-
-		{
+		canvas.setOnMouseDragged(mouse -> {
 			gc.stroke();
 			gc.lineTo(mouse.getX(), mouse.getY());
 		});
@@ -36,7 +49,12 @@ public class DrawApp extends Screen {
 			gc.closePath();
 		});
 
-		components.add(canvas);
+		grid.add(canvas, 0, 0);
+		grid.add(paintBrushBtn, 0, 1);
+		grid.add(paintBucketBtn, 1, 1);
+		grid.add(cPicker, 2, 0);
+		
+		components.add(grid);
 
 	}
 
