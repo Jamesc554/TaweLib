b9d96acf4659f327e6d7abfee2999da097af32ba
James Carter <jamesc554544@gmail.com>
2018-11-19 17:20:52 +0000

    Start of JavaDoc for Sort Class

--- src/utils/Sort.java
+++ src/utils/Sort.java
@@ -7,18 +7,38 @@ import java.util.List;
 
 public class Sort {
 
+    /**
+     * @param resources
+     * @param ascending
+     * @return
+     */
     public static List<Resource> sortResourcesByName(List<Resource> resources, boolean ascending){
         return null;
     }
 
+    /**
+     * @param resources
+     * @param ascending
+     * @return
+     */
     public static List<Resource> sortResourcesByYear(List<Resource> resources, boolean ascending){
         return null;
     }
 
+    /**
+     * @param resources
+     * @param ascending
+     * @return
+     */
     public static List<Resource> sortBooksByAuthor(List<Book> resources, boolean ascending){
         return null;
     }
 
+    /**
+     * @param resources
+     * @param ascending
+     * @return
+     */
     public static List<Resource> sortBooksByPublisher(List<Book> resources, boolean ascending){
         return null;
     }
