a2cec3f235364338fe98d26ce93257feb509f5d4
PeterDaish <965261@swansea.ac.uk>
2018-11-19 13:10:55 +0000

    Added javadoc + gets and sets for all functions of Book

--- src/resources/Book.java
+++ src/resources/Book.java
@@ -310,50 +310,108 @@ public class Book extends Resource {
 	 * #############################################################
 	 */
 	
+	/**
+	 * Returns the author of the book
+	 * @return author
+	 */
 	public String getAuthor() {
 		return author;
 	}
-
+	/**
+	 * Sets the author of this book
+	 * @param author 
+	 * The new author of this book
+	 */
 	public void setAuthor(String author) {
 		this.author = author;
 	}
 
+	/**
+	 * Returns the publisher of this book
+	 * @return publisher
+	 * The publisher of this book
+	 */
 	public String getPublisher() {
 		return publisher;
 	}
 
+	/**
+	 * Sets the publisher of this book
+	 * @param publisher
+	 * The new publisher of this book
+	 */
 	public void setPublisher(String publisher) {
 		this.publisher = publisher;
 	}
 
+	/**
+	 * Gets the genre of this book
+	 * @return genre
+	 * The genre of this book.
+	 */
 	public String getGenre() {
 		return genre;
 	}
 
+	/**
+	 * Sets the genre of this book
+	 * @param genre
+	 * The new genre of this book.
+	 */
 	public void setGenre(String genre) {
 		this.genre = genre;
 	}
 
+	/**
+	 * Gets the ISBN of this book
+	 * @return isbn
+	 * The isbn of this book
+	 */
 	public String getIsbn() {
 		return isbn;
 	}
 
+	/**
+	 * Sets the ISBN of this book
+	 * @param isbn
+	 * The new ISBN of this book.
+	 */
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
 
+	/**
+	 * Gets the languages this book is available in
+	 * @return languages
+	 * The languages this book is available in.
+	 */
 	public ArrayList<String> getLanguages() {
 		return languages;
 	}
 
+	/**
+	 * Sets the languages this book is available in.
+	 * @param languages
+	 * The languages this book will become available in.
+	 */
 	public void setLanguages(ArrayList<String> languages) {
 		this.languages = languages;
 	}
 	
+	/**
+	 * Adds a new language to this book
+	 * @param newLang
+	 * The new languages to be added.
+	 */
 	public void addLanguage(String newLang) {
 		this.languages.add(newLang);
 	}
 	
+	/**
+	 * Removes a specific languages from this book
+	 * @param langToRemove
+	 * The language to be removed.
+	 */
 	public void removeLanguage(String langToRemove) {
 		//search through arrayList and remove language selected.
 		for (int i = 0; i < this.languages.size(); i++) {
