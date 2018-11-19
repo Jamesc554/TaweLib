019519a66672271dcd78f611ef00a0ac0656a1a8
James Carter <jamesc554544@gmail.com>
2018-11-19 16:59:15 +0000

    Added buttons to TestScreen so you can naviagte to every screen on the application

--- src/screen/TestScreen.java
+++ src/screen/TestScreen.java
@@ -10,14 +10,50 @@ public class TestScreen extends Screen {
     public void start() {
         components = new ArrayList<>();
 
-        Button btn = new Button();
-        btn.setText("Go to DrawApp");
+        Button drawAppBtn = new Button();
+        drawAppBtn.setText("Go to DrawApp");
         
-        btn.setOnAction(e->{
+        drawAppBtn.setOnAction(e->{
         	ScreenManager.changeScreen(new DrawApp());
         });
         
-        components.add(btn);
+        components.add(drawAppBtn);
+        
+        Button homeScreenBtn = new Button();
+        homeScreenBtn.setText("Go to HomeScreen");
+        
+        homeScreenBtn.setOnAction(e->{
+        	ScreenManager.changeScreen(new HomeScreen());
+        });
+        
+        components.add(homeScreenBtn);
+        
+        Button issueDeskBtn = new Button();
+        issueDeskBtn.setText("Go to IssueDesk");
+        
+        issueDeskBtn.setOnAction(e->{
+        	ScreenManager.changeScreen(new IssueDesk());
+        });
+        
+        components.add(issueDeskBtn);
+        
+        Button resourceScreenBtn = new Button();
+        resourceScreenBtn.setText("Go to ResourceScreen");
+        
+        resourceScreenBtn.setOnAction(e->{
+        	ScreenManager.changeScreen(new ResourceScreen());
+        });
+        
+        components.add(resourceScreenBtn);
+        
+        Button searchResultScreen = new Button();
+        searchResultScreen.setText("Go to DrawApp");
+        
+        searchResultScreen.setOnAction(e->{
+        	ScreenManager.changeScreen(new SearchResultScreen());
+        });
+        
+        components.add(searchResultScreen);
     }
 
     @Override
