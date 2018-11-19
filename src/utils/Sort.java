185ad29f52681a023271476449e581ad8a3d1084
James Carter <jamesc554544@gmail.com>
2018-11-19 13:44:39 +0000

    Added some outline for Sort class

---
+++ src/utils/Sort.java
@@ -0,0 +1,26 @@
+package utils;
+
+import resources.Book;
+import resources.Resource;
+
+import java.util.List;
+
+public class Sort {
+
+    public static List<Resource> sortResourcesByName(List<Resource> resources, boolean ascending){
+        return null;
+    }
+
+    public static List<Resource> sortResourcesByYear(List<Resource> resources, boolean ascending){
+        return null;
+    }
+
+    public static List<Resource> sortBooksByAuthor(List<Book> resources, boolean ascending){
+        return null;
+    }
+
+    public static List<Resource> sortBooksByPublisher(List<Book> resources, boolean ascending){
+        return null;
+    }
+
+}
