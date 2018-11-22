573d8b2604cefeffea857f28ec79de1983ca5acc
SoniCKill <dominikwojtasiewicz@gmail.com>
2018-11-22 12:08:18 +0000

    Crated all methods name in Library. Implemented LibRes

--- src/library/Library.java
+++ src/library/Library.java
@@ -1,4 +1,95 @@
 package library;
+import user.*;
+import resources.*;
+import io.*;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 
 public class Library {
+	private LibraryResources libraryResources = new LibraryResources();
+
+	Library(){ };
+
+	public Book createBook(String year,String title, String thumbnailImg, String uniqueID,
+						   String author, String genre, String isbn, String publisher, ArrayList<String> lang){
+		return new Book(year, title, thumbnailImg, uniqueID, author, genre, isbn, publisher, lang);
+	}
+	public DVD createDVD(String year, String title, String thumbnailImageRef, String uniqueID,
+						 String director, String runtime, String language, ArrayList<String> subLang){
+		return new DVD(year, title, thumbnailImageRef, uniqueID, director, runtime, language, subLang);
+	}
+	public Laptop creatLaptop(String year, String title, String thumbnailImageRef, String uniqueID,
+							  String manufacturer, String model,  String operatingSys){
+		return new Laptop(year, title, thumbnailImageRef, uniqueID, manufacturer, model, operatingSys);
+	}
+	public User creatUser(String userName, String firstName, String lastName, String mobileNumber, String firstLineAddress,
+						  String secondLineAddress, String postCode, String townName, int accountBalance, String profImage){
+		return new User(userName, firstName, lastName, mobileNumber, firstLineAddress,
+				secondLineAddress, postCode, townName, accountBalance, profImage);
+	}
+	public User creatLibrarian(String userName, String firstName, String lastName, String mobileNumber, String firstLineAddress,
+								  String secondLineAddress, String postCode, String townName, int accountBalance, String profImage,
+								  int empDay, int empMonth, int empYear, String staffNumber, int noOfEmploys){
+		return new Librarian(userName, firstName, lastName, mobileNumber, firstLineAddress, secondLineAddress,
+				postCode, townName, accountBalance, profImage, empDay, empMonth, empYear, staffNumber, noOfEmploys);
+	}
+	public Resource getResource(String id){
+		String resourceType = id.substring(0, 1);
+		switch (resourceType.toLowerCase()){
+			case "l":
+				return libraryResources.getLaptop(id);
+			case "d":
+				return libraryResources.getDVD(id);
+			case "b":
+				return libraryResources.getBook(id);
+			default:
+				return null;
+		}
+	}
+	public User getUser(String username){
+		return libraryResources.getUser(username);
+	}
+	public void addBalance(int amount, String username){
+		getUser(username).addAccountBalance(amount);
+	}
+	public void subtractBalance(int amount, String username){
+		getUser(username).addAccountBalance(amount);
+	}
+	public String getCurrentDateTime(){
+		SimpleDateFormat dataFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		return dataFormat.format(new Date());
+
+	}
+	public void loanResource(String username, String resourceID){
+		getUser(username).addResource(getResource(resourceID));
+	}
+	public void removeResource(String id){}
+	public void removeUser(String username){}
+	public void shutDown(){}
+	public Boolean checkForUser(){
+		return null; //placeholder
+	}
+	public ArrayList<Book> getAllBooks(){
+		return null; //placeholder
+	}
+	public ArrayList<Laptop> getAllLaptops(){
+		return null; //placeholder}
+	}
+	public ArrayList<DVD> getAllDVD(){
+		return null; //placeholder
+	}
+	public ArrayList<User> getAllUsers(){
+		return null; //placeholder
+	}
+	public void changeAddress(String username, String firstLine, String secondLine){}
+	public void changePoctCode(String username, String postCode){}
+	public void changeTownName(String username, String townName){}
+	public void changePhoneNumber(String username, String phoneNumber){}
+	public void changeLastName(String username, String lastname){}
+	public void changeImage(String username, String path){}
+	public void searchResources(String text){}
+
 }
--- src/library/LibraryResources.java
+++ src/library/LibraryResources.java
@@ -1,9 +1,74 @@
 package library;
 import resources.*;
+import user.*;
 
 import java.util.ArrayList;
 
 public class LibraryResources {
 	ArrayList<Book> listOfBooks = new ArrayList<>();
-	ArrayList<Laptop> list
+	ArrayList<Laptop> listOfLaptops = new ArrayList<>();
+	ArrayList<DVD> listOfDVD = new ArrayList<>();
+	ArrayList<User> listOfUsers = new ArrayList<>();
+
+	public LibraryResources(){}
+
+	public void addBook(Book book){
+		listOfBooks.add(book);
+	}
+	public void addLaptop(Laptop laptop){
+		listOfLaptops.add(laptop);
+	}
+	public void addDVD(DVD dvd){
+		listOfDVD.add(dvd);
+	}
+	public void addUser(User user){
+		listOfUsers.add(user);
+	}
+	protected User getUser(String username){
+		for(User u : this.listOfUsers){
+			if(u.getUserName().equals(username)){
+				return u;
+			}
+		}
+		return null;
+	}
+	protected Book getBook(String id){
+		for(Book b : this.listOfBooks){
+			if(b.getUniqueID().equals(id)){
+				return b;
+			}
+		}
+		return null;
+	}
+	protected DVD getDVD(String id){
+		for(DVD d : this.listOfDVD){
+			if(d.getUniqueID().equals(id)){
+				return d;
+			}
+		}
+		return null;
+	}
+	protected Laptop getLaptop(String id){
+		for(Laptop l : this.listOfLaptops){
+			if(l.getUniqueID().equals(id)){
+				return l;
+			}
+		}
+		return null;
+	}
+	public ArrayList<User> getAllUsers(){
+		return this.listOfUsers;
+	}
+
+	public ArrayList<Book> getListOfBooks() {
+		return this.listOfBooks;
+	}
+
+	public ArrayList<DVD> getListOfDVD() {
+		return this.listOfDVD;
+	}
+
+	public ArrayList<Laptop> getListOfLaptops() {
+		return this.listOfLaptops;
+	}
 }
