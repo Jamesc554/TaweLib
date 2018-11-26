ce5a1037c81154b22e06b9504ea439934bd47d2e
Dominik <dominikwojtasiewicz@gmail.com>
2018-11-26 17:05:38 +0000

    Removed LibraryResources libraryResources = new LibraryResource fixed all other warning

--- src/library/Library.java
+++ src/library/Library.java
@@ -9,32 +9,31 @@ import java.util.Date;
 
 
 public class Library {
-	private static LibraryResources libraryResources = new LibraryResources();
 	private static String currentUser;
 
 	Library(){ }
 
 	public static void addBook(String year,String title, String thumbnailImg, String uniqueID,
 						   String author, String genre, String isbn, String publisher, ArrayList<String> lang){
-		libraryResources.addBook(new Book(year, title, thumbnailImg, uniqueID, author, genre, isbn, publisher, lang));
+		LibraryResources.addBook(new Book(year, title, thumbnailImg, uniqueID, author, genre, isbn, publisher, lang));
 	}
 	public static void addDVD(String year, String title, String thumbnailImageRef, String uniqueID,
 						 String director, String runtime, String language, ArrayList<String> subLang){
-	    libraryResources.addDVD(new DVD(year, title, thumbnailImageRef, uniqueID, director, runtime, language, subLang));
+	    LibraryResources.addDVD(new DVD(year, title, thumbnailImageRef, uniqueID, director, runtime, language, subLang));
 	}
 	public static void addLaptop(String year, String title, String thumbnailImageRef, String uniqueID,
 							  String manufacturer, String model,  String operatingSys){
-	    libraryResources.addLaptop(new Laptop(year, title, thumbnailImageRef, uniqueID, manufacturer, model, operatingSys));
+	    LibraryResources.addLaptop(new Laptop(year, title, thumbnailImageRef, uniqueID, manufacturer, model, operatingSys));
 	}
 	public static void addUser(String userName, String firstName, String lastName, String mobileNumber, String firstLineAddress,
 						  String secondLineAddress, String postCode, String townName, int accountBalance, String profImage){
-		libraryResources.addUser(new User(userName, firstName, lastName, mobileNumber, firstLineAddress,
+		LibraryResources.addUser(new User(userName, firstName, lastName, mobileNumber, firstLineAddress,
                 secondLineAddress, postCode, townName, accountBalance, profImage));
 	}
 	public static void addLibrarian(String userName, String firstName, String lastName, String mobileNumber, String firstLineAddress,
 								  String secondLineAddress, String postCode, String townName, int accountBalance, String profImage,
 								  int empDay, int empMonth, int empYear, String staffNumber, int noOfEmploys){
-		libraryResources.addUser(new Librarian(userName, firstName, lastName, mobileNumber, firstLineAddress,
+		LibraryResources.addUser(new Librarian(userName, firstName, lastName, mobileNumber, firstLineAddress,
                 secondLineAddress, postCode, townName, accountBalance, profImage, empDay, empMonth, empYear, staffNumber,
                 noOfEmploys));
 	}
@@ -42,17 +41,17 @@ public class Library {
 		String resourceType = id.substring(0, 1);
 		switch (resourceType.toLowerCase()){
 			case "l":
-				return libraryResources.getLaptop(id);
+				return LibraryResources.getLaptop(id);
 			case "d":
-				return libraryResources.getDVD(id);
+				return LibraryResources.getDVD(id);
 			case "b":
-				return libraryResources.getBook(id);
+				return LibraryResources.getBook(id);
 			default:
 				return null;
 		}
 	}
 	public static User getUser(String username){
-		return libraryResources.getUser(username);
+		return LibraryResources.getUser(username);
 	}
 	public static void addBalance(int amount, String username){
 		getUser(username).addAccountBalance(amount);
@@ -72,28 +71,28 @@ public class Library {
 		getUser(username).returnResource(resourceID);
 	}
 	public static void removeResource(String id){
-        libraryResources.removeResource(id);
+        LibraryResources.removeResource(id);
     }
 	public static void removeUser(String username){
-	    libraryResources.removeUser(username);
+	    LibraryResources.removeUser(username);
 	}
 	public static void shutDown(){}
 
 	public static boolean checkForUser(String username){
-		return libraryResources.checkIfValidUsername(username);
+		return LibraryResources.checkIfValidUsername(username);
 	}
 
 	public static ArrayList<Book> getAllBooks(){
-		return libraryResources.getListOfBooks();
+		return LibraryResources.getListOfBooks();
 	}
 	public ArrayList<Laptop> getAllLaptops(){
-		return libraryResources.getListOfLaptops();
+		return LibraryResources.getListOfLaptops();
 	}
 	public static ArrayList<DVD> getAllDVD(){
-		return libraryResources.getListOfDVD();
+		return LibraryResources.getListOfDVD();
 	}
 	public static ArrayList<User> getAllUsers(){
-		return libraryResources.getAllUsers();
+		return LibraryResources.getAllUsers();
 	}
 
 	// TODO: CHANGE FROM STRING TO USER ONCE IMPLEMENTED
