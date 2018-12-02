4c2f21eacf2b4a1de8538fcc35348979a97aafa5
Dominik <dominikwojtasiewicz@gmail.com>
2018-12-02 14:54:28 +0000

    Added java doc. Java doc for Library, Libraryresources, User and Librarian is done.

--- src/library/Library.java
+++ src/library/Library.java
@@ -7,31 +7,107 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * This class implements all methods required for library operations.
+ * @author Dominik R Wojtasiewicz
+ * @since 18/11/2018
+ */
 
 public class Library {
-	private static User currentUser;
+	private static User currentUser; //Stores the currently logged-in user.
 
+	/**
+	 * This methods starts the library. Library is a static method so we can only
+	 * have one at a time.
+	 */
 	public static void start(){
 		LibraryResources.start();
 	}
 
+	/**
+	 * This methods adds a new book to the library.
+	 * @param year	Year published.
+	 * @param title Book title.
+	 * @param thumbnailImg Path to the image of the book.
+	 * @param uniqueID ID of the book.
+	 * @param author The author of the book.
+	 * @param genre The genre of the book.
+	 * @param isbn ISBN of the book.
+	 * @param publisher The name of the published.
+	 * @param lang Language of the book.
+	 */
 	public static void addBook(String year,String title, String thumbnailImg, String uniqueID,
 						   String author, String genre, String isbn, String publisher, ArrayList<String> lang){
 		LibraryResources.addBook(new Book(year, title, thumbnailImg, uniqueID, author, genre, isbn, publisher, lang));
 	}
-	public static void addDVD(String year, String title, String thumbnailImageRef, String uniqueID,
+
+	/**
+	 * This methods adds a new DVD to the library.
+	 * @param year	Year released .
+	 * @param title Book title.
+	 * @param thumbnailImg Path to the image of the DVD.
+	 * @param uniqueID ID of the DVD.
+	 * @param director The name of the director.
+	 * @param runtime The run time of the movie.
+	 * @param language The language of the movie.
+	 * @param subLang The subtitles language of the DVD.
+	 */
+	public static void addDVD(String year, String title, String thumbnailImg, String uniqueID,
 						 String director, String runtime, String language, ArrayList<String> subLang){
-	    LibraryResources.addDVD(new DVD(year, title, thumbnailImageRef, subLang, director, runtime, language, uniqueID));
+	    LibraryResources.addDVD(new DVD(year, title, thumbnailImg, subLang, director, runtime, language, uniqueID));
 	}
+
+	/**
+	 * This methods adds a laptop to the library.
+	 * @param year The year of release.
+	 * @param title The name of the laptop.
+	 * @param thumbnailImageRef Img of the laptop.
+	 * @param uniqueID The id of the laptop.
+	 * @param manufacturer The manufacture of the laptop.
+	 * @param model The model of the laptop.
+	 * @param operatingSys The operating system of the laptop.
+	 */
 	public static void addLaptop(String year, String title, String thumbnailImageRef, String uniqueID,
 							  String manufacturer, String model,  String operatingSys){
 	    LibraryResources.addLaptop(new Laptop(year, title, thumbnailImageRef, uniqueID, manufacturer, model, operatingSys));
 	}
+
+	/**
+	 * This methods adds a user to the library.
+	 * @param userName the username.
+	 * @param firstName first name.
+	 * @param lastName last name.
+	 * @param mobileNumber mobile number.
+	 * @param firstLineAddress first line of address.
+	 * @param secondLineAddress second line of address.
+	 * @param postCode post code.
+	 * @param townName name of the town.
+	 * @param accountBalance account balance
+	 * @param profImage path to the profile image of the user.
+	 */
 	public static void addUser(String userName, String firstName, String lastName, String mobileNumber, String firstLineAddress,
 						  String secondLineAddress, String postCode, String townName, int accountBalance, String profImage){
 		LibraryResources.addUser(new User(userName, firstName, lastName, mobileNumber, firstLineAddress,
                 secondLineAddress, postCode, townName, accountBalance, profImage));
 	}
+	/**
+	 * This methods adds a librarian to the library.
+	 * @param userName the username.
+	 * @param firstName first name.
+	 * @param lastName last name.
+	 * @param mobileNumber mobile number.
+	 * @param firstLineAddress first line of address.
+	 * @param secondLineAddress second line of address.
+	 * @param postCode post code.
+	 * @param townName name of the town.
+	 * @param accountBalance account balance
+	 * @param profImage path to the profile image of the librarian.
+	 * @param empDay the day of employment start.
+	 * @param empMonth the month of employment start.
+	 * @param empYear the year of employment start.
+	 * @param staffNumber staff number.
+	 * @param noOfEmploys number of employs.
+	 */
 	public static void addLibrarian(String userName, String firstName, String lastName, String mobileNumber, String firstLineAddress,
 								  String secondLineAddress, String postCode, String townName, int accountBalance, String profImage,
 								  int empDay, int empMonth, int empYear, String staffNumber, int noOfEmploys){
@@ -39,6 +115,12 @@ public class Library {
                 secondLineAddress, postCode, townName, accountBalance, profImage, empDay, empMonth, empYear, staffNumber,
                 noOfEmploys));
 	}
+
+	/**
+	 * Get's resource when the id of sed resource is known. Used for all resources.
+	 * @param id String of ID of the resource.
+	 * @return Resource object.
+	 */
 	public static Resource getResource(String id){
 		String resourceType = id.substring(0, 1);
 		switch (resourceType.toLowerCase()){
@@ -52,13 +134,28 @@ public class Library {
 				return null;
 		}
 	}
+	/**
+	 * Gets the user based on username.
+	 * @param username String username.
+	 * @return user object.
+	 */
 	public static User getUser(String username){
 		return LibraryResources.getUser(username);
 	}
+	/**
+	 * Adds balance to the user account.
+	 * @param amount amount to increase the balance.
+	 * @param username the username to modify.
+	 */
 	public static void addBalance(int amount, String username){
 		getUser(username).addAccountBalance(amount);
 	}
 
+	/**
+	 * Removes balance from account.
+	 * @param amount amount to be removed.
+	 * @param username username of the user.
+	 */
 	public static void subtractBalance (int amount, String username) {
 		if (amount <= 0 ) {
 			throw new IllegalArgumentException("Cannot subtract negative or null amount");
@@ -68,46 +165,107 @@ public class Library {
 		getUser(username).subtractAccountBalance(amount);
 	}
 
+	/**
+	 * Returns current date and time in the following format DD-MM-YYYY HH-MM-SS
+	 * @return String.
+	 */
 	public static String getCurrentDateTime(){
 		SimpleDateFormat dataFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 		return dataFormat.format(new Date());
 
 	}
+
+	/**
+	 * Loans resource to the user.
+	 * @param username Username of the person borrowing a resource.
+	 * @param resourceID The id of the resource.
+	 */
 	public static void loanResource(String username, String resourceID){
 		getUser(username).loanResource(resourceID);
+	 	//TODO ADD THE USER TO THE CURRENTLY OUT HASH MAP IN RESOURCE
 	}
+
+	/**
+	 * Returns resource to the library.
+	 * @param username Username
+	 * @param resourceID ID of the resource.
+	 */
 	public static void returnResource(String username, String resourceID){
 		getUser(username).returnResource(resourceID);
+		//TODO REMOVE THE USER FROM CURRENTLY OUT HASH MAP IN RESOURCE
 	}
+
+	/**
+	 * This removes a resource from library.
+	 * @param id Unique ID of the resource.
+	 */
 	public static void removeResource(String id){
         LibraryResources.removeResource(id);
     }
+
+	/**
+	 * Remove user from the library.
+	 * @param username
+	 */
 	public static void removeUser(String username){
 	    LibraryResources.removeUser(username);
 	}
+
+	/**
+	 * This methods shuts down the library.
+	 */
 	public static void shutDown(){}
 
+	/**
+	 * Checks if user exists.
+	 * @param username Username of the user
+	 * @return Boolean. True if exists False if not.
+	 */
 	public static boolean checkForUser(String username){
 		return LibraryResources.checkIfValidUsername(username);
 	}
 
+	/**
+	 * Returns all books in the library.
+	 * @return ArrayList<Book> of all books.
+	 */
 	public static ArrayList<Book> getAllBooks(){
 		return LibraryResources.getListOfBooks();
 	}
+	/**
+	 * Returns all Laptops in the library.
+	 * @return ArrayList<Laptop> of all books.
+	 */
 	public static ArrayList<Laptop> getAllLaptops(){
 		return LibraryResources.getListOfLaptops();
 	}
+	/**
+	 * Returns all books in the DVD's.
+	 * @return ArrayList<DVD> of all books.
+	 */
 	public static ArrayList<DVD> getAllDVD(){
 		return LibraryResources.getListOfDVD();
 	}
+	/**
+	 * Returns all users in the library.
+	 * @return ArrayList<User> of all books.
+	 */
 	public static ArrayList<User> getAllUsers(){
 		return LibraryResources.getAllUsers();
 	}
 
+	/**
+	 * Sets the current logged in user.
+	 * @param user the user object.
+	 */
 	public static void setLoggedInUser(User user) {
 		currentUser = user;
 	}
 
+	/**
+	 * Gets the currently logged in user.
+	 * @return User object.
+	 */
 	public static User getCurrentLoggedInUser() {
 		return currentUser;
 	}
@@ -120,26 +278,68 @@ public class Library {
 		return currentUser instanceof Librarian;
 	}
 
+	/**
+	 * Changes address of the user.
+	 * @param username The username of the user for which the address should be changed.
+	 * @param firstLine The first line of address.
+	 * @param secondLine Second line of address.
+	 */
 	public static void changeAddress(String username, String firstLine, String secondLine){
 	    User u = getUser(username);
 	    u.setFirstLineAddress(firstLine);
 	    u.setSecondLineAddress(secondLine);
     }
+
+	/**
+	 * Changes the post code of the user.
+	 * @param username The username of the user for which the address should be changed.
+	 * @param postCode
+	 */
 	public static void changePoctCode(String username, String postCode){
 	    getUser(username).setPostCode(postCode);
     }
+
+	/**
+	 * Changes the post code of the user.
+	 * @param username The username of the user for which the address should be changed.
+	 * @param townName The new town name.
+	 */
 	public static void changeTownName(String username, String townName){
 	    getUser(username).setTownName(townName);
     }
+
+	/**
+	 * Changes the post code of the user.
+	 * @param username The username of the user for which the address should be changed.
+	 * @param phoneNumber The new phone number.
+	 */
 	public static void changePhoneNumber(String username, String phoneNumber){
 	    getUser(username).setMobileNumber(phoneNumber);
     }
+
+	/**
+	 * Changes the last name of the user.
+	 * @param username The username of which data will be updated.
+	 * @param lastname The new last name.
+	 */
 	public static void changeLastName(String username, String lastname){
 	    getUser(username).setLastName(lastname);
     }
+
+	/**
+	 * Changes the profile img of the user.
+	 * @param username The username of the user for which the address should be changed.
+	 * @param path the new path of the profile image.
+	 */
 	public static void changeImage(String username, String path){
 	    getUser(username).setProfImage(path);
     }
+
+	/**
+	 * User toString.
+	 * @param username username.
+	 * @return single string will all user information.
+	 */
 	public static String userToString(String username){
 		User u = getUser(username);
 	    String info = u.getUserName()+ "\n";
@@ -157,6 +357,7 @@ public class Library {
 	return info;
     }
 
+	//TODO Remove or modify. Write JavaDoc.
     public static Resource getResourceById(String type, String id) {
 	    Resource r;
 	    if (type.equals("Book")) {
@@ -174,35 +375,44 @@ public class Library {
         }
     }
 
+	/**
+	 * This method looks up all resource based on partial string.
+	 * @param text partial search parameter.
+	 * @return ArrayList<Resource> of object that match the search query.
+	 */
 	public static ArrayList<Resource> searchResources(String text){
-	    int textSize = text.length();
+		//Sets size of the search for hashing.
+		int textSize = text.length();
+		//Hashes the text we searching.
 	    Integer hashedTextSearch = hashText(text.toLowerCase());
 
-	    ArrayList<String> textPart = new ArrayList();
-        ArrayList<Integer> hashedTextPart = new ArrayList();
+	    //Get's all data needed for search.
+	    ArrayList<String> textPart;
+        ArrayList<Integer> hashedTextPart;
         ArrayList<Resource> resources = new ArrayList<>();
         ArrayList<Resource> result = new ArrayList<>();
 
-
+        //Add's all needed data.
         resources.addAll(LibraryResources.getListOfBooks());
         resources.addAll(LibraryResources.getListOfDVD());
         resources.addAll(LibraryResources.getListOfLaptops());
 
         int i = 0;
-        
-//        for (Resource r : resources) {
-//        	if (r.getTitle().contains(text)) {
-//        		
-//        	}
-//        }
 
+        //While there are resources that have not been checked.
         while(i < resources.size()){
+        	//Divides the text into substring for hashing.
             textPart = partString(resources.get(i), textSize);
+            //Hashes substrings.
             hashedTextPart = hashList(textPart);
             int j = 0;
+            //While the are parts the need to be compared
             while(j < hashedTextPart.size()) {
+            	//If the hashed numbers are the same.
                 if(hashedTextPart.get(j).equals(hashedTextSearch)){
+                	//If the corresponding string are the same
                     if(textPart.get(j).equals(text.toLowerCase())){
+                    	//Add said resource to list
                         result.add(resources.get(i));
                     }
                 }
@@ -212,6 +422,13 @@ public class Library {
         }
         return result;
     }
+
+	/**
+	 * This methods divides object information into substring for hashing.
+	 * @param res The resource to be split.
+	 * @param partSize The part size of the list
+	 * @return ArrayList<String> of strings.
+	 */
     private static ArrayList<String> partString(Resource res, int partSize){
 	    ArrayList<String> list = new ArrayList<>();
 	    String resString = res.toSingleString();
@@ -223,19 +440,28 @@ public class Library {
         list.add(resString.substring(resString.length()-partSize,resString.length()).toLowerCase());
         return list;
     }
-    private static ArrayList<Integer> hashList(ArrayList<String> parts){
+
+	/**
+	 * This methods hashed the substring into an arrayList.
+	 * @param parts ArrayList<String> to be hashed.
+	 * @return ArrayList<Integer> containing hashed data.
+	 */
+	private static ArrayList<Integer> hashList(ArrayList<String> parts){
 	    ArrayList<Integer> list = new ArrayList<>();
 	    Integer sum;
 	    for(String p : parts){
-	        sum = 0;
-	        for(int i=0; i < p.length(); i++){
-	            sum += (int)p.charAt(i);
-            }
-            list.add(sum);
+	        sum = hashText(p);
+	        list.add(sum);
         }
         return list;
     }
-    private static Integer hashText(String text){
+
+	/**
+	 * Hashes a string into number.
+	 * @param text string to be hashed.
+	 * @return Integer representation of the number.
+	 */
+	private static Integer hashText(String text){
 	    Integer sum = 0;
 	    String t = text.toLowerCase();
 	    for(int i=0; i<t.length(); i++){
--- src/library/LibraryResources.java
+++ src/library/LibraryResources.java
@@ -2,32 +2,67 @@ package library;
 import resources.*;
 import user.*;
 import io.*;
-
+/**
+ * This class implements the LibraryResources database. This class is static.
+ * @author Dominik Wojtasiewicz
+ * @since 18/11/2018
+ */
 import java.util.ArrayList;
 
 public class LibraryResources {
-	private static ArrayList<Book> listOfBooks = new ArrayList<>();
-	private static ArrayList<Laptop> listOfLaptops = new ArrayList<>();
-    private static ArrayList<DVD> listOfDVD = new ArrayList<>();
-    private static ArrayList<User> listOfUsers = new ArrayList<>();
+	private static ArrayList<Book> listOfBooks = new ArrayList<>(); //List of books
+	private static ArrayList<Laptop> listOfLaptops = new ArrayList<>(); //List of laptops
+    private static ArrayList<DVD> listOfDVD = new ArrayList<>(); //List of dvd's
+    private static ArrayList<User> listOfUsers = new ArrayList<>(); //List of users
 
+	/**
+	 * On start create all users, dvd, books, laptops.
+	 */
+	//TODO add books and laptops
 	public static void start(){
 		listOfUsers = ReadFile.readUsers();
 		listOfDVD = ReadFile.readDvds();
+		//listOfBooks = ReadFile.readBooks();
+		//listOfLaptops = ReadFile.readLaptops();
 	}
 
+	/**
+	 * Adds a book to the libraryResource.
+	 * @param book Book object.
+	 */
 	public static void addBook(Book book){
 		listOfBooks.add(book);
 	}
+
+	/**
+	 * Adds a laptop to the libraryResource.
+	 * @param laptop Laptop object.
+	 */
 	public static void addLaptop(Laptop laptop){
 		listOfLaptops.add(laptop);
 	}
+
+	/**
+	 * Adds a dvd to the libraryResources.
+	 * @param dvd DVD object.
+	 */
 	public static void addDVD(DVD dvd){
 		listOfDVD.add(dvd);
 	}
+
+	/**
+	 * Adds a user to the libraryResources.
+	 * @param user a User object.
+	 */
 	public static void addUser(User user){
 		listOfUsers.add(user);
 	}
+
+	/**
+	 * Get's a user form the database
+	 * @param username the username.
+	 * @return a User object.
+	 */
 	protected static User getUser(String username){
 		for(User u : listOfUsers){
 			if(u.getUserName().equals(username)){
@@ -37,7 +72,12 @@ public class LibraryResources {
 		return null;
 	}
 
-	public static boolean checkIfValidUsername(String username){
+	/**
+	 * Checks if a user is valid.
+	 * @param username
+	 * @return
+	 */
+	protected static boolean checkIfValidUsername(String username){
 		if (listOfUsers.isEmpty())
 			return false;
 
@@ -49,6 +89,11 @@ public class LibraryResources {
 		return false;
 	}
 
+	/**
+	 * Gets a book from list based on id.
+	 * @param id of the book.
+	 * @return Book object. Returns null if no such book.
+	 */
 	protected static Book getBook(String id){
 		for(Book b : listOfBooks){
 			if(b.getUniqueID().equals(id)){
@@ -57,6 +102,12 @@ public class LibraryResources {
 		}
 		return null;
 	}
+
+	/**
+	 * Gets a DVD from list based on id.
+	 * @param id of the DVD.
+	 * @return DVD object. Null if no such object.
+	 */
 	protected static DVD getDVD(String id){
 		for(DVD d : listOfDVD){
 			if(d.getUniqueID().equals(id)){
@@ -65,6 +116,12 @@ public class LibraryResources {
 		}
 		return null;
 	}
+
+	/**
+	 * Gets a Laptop from list based on id.
+	 * @param id of the Laptop.
+	 * @return Laptop object. Null if no such object.
+	 */
 	protected static Laptop getLaptop(String id){
 		for(Laptop l : listOfLaptops){
 			if(l.getUniqueID().equals(id)){
@@ -73,22 +130,44 @@ public class LibraryResources {
 		}
 		return null;
 	}
-	public static ArrayList<User> getAllUsers(){
+
+	/**
+	 * Returns all users
+	 * @return ArrayList<User> of users.
+	 */
+	protected static ArrayList<User> getAllUsers(){
 		return listOfUsers;
 	}
 
-	public static ArrayList<Book> getListOfBooks() {
+	/**
+	 * Returns all books.
+	 * @return ArrayList<Book> of books.
+	 */
+	protected static ArrayList<Book> getListOfBooks() {
 		return listOfBooks;
 	}
 
-	public static ArrayList<DVD> getListOfDVD() {
+	/**
+	 * Returns all DVD.
+	 * @return ArrayList<DVD> of DVDs.
+	 */
+	protected static ArrayList<DVD> getListOfDVD() {
 		return listOfDVD;
 	}
 
-	public static ArrayList<Laptop> getListOfLaptops() {
+	/**
+	 * Returns all Laptops.
+	 * @return ArrayList<Laptop> of Laptops.
+	 */
+	protected static ArrayList<Laptop> getListOfLaptops() {
 		return listOfLaptops;
 	}
-	public static void removeResource(String id){
+
+	/**
+	 * Removes a resource from LibraryResources.
+	 * @param id of the resource to be removed.
+	 */
+	protected static void removeResource(String id){
 		String dataType = id.substring(0,1);
 		switch (dataType.toLowerCase()){
 			case "b":
@@ -120,7 +199,12 @@ public class LibraryResources {
 		}
 
 	}
-	public static void removeUser(String username){
+
+	/**
+	 * Removes the user from LibraryResources.
+	 * @param username of the user to be removed.
+	 */
+	protected static void removeUser(String username){
         for(int index = 0; index < listOfUsers.size(); index++) {
             if (listOfUsers.get(index).getUserName().equals(username)) {
                 listOfUsers.remove(index);
