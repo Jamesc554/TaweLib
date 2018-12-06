1dfe6b212003ad2873a28f359e8f5bfcb0a781d7
James Carter <jamesc554544@gmail.com>
2018-12-06 17:38:53 +0000

    Changed BorrowHistory to List

--- src/resources/CopyData.java
+++ src/resources/CopyData.java
@@ -1,17 +1,18 @@
 package resources;
 
+import java.util.List;
+
 public class CopyData {
 	
 	private String id;
-	private BorrowHistoryData[] borrowHistory; // [4] [0] - User ID [1] - date borrowed [2] - date returned [3] - requested return date
+	private List<BorrowHistoryData> borrowHistory; // [4] [0] - User ID [1] - date borrowed [2] - date returned [3] - requested return date
 	private BorrowHistoryData currentInfo; // [4] [0] - User ID [1] - date borrowed [2] - date returned [3] - requested return date
 	private String loanDuration;
 	
-	public CopyData(String copyID, BorrowHistoryData[] borrowHistory, String loanDuration) {
+	public CopyData(String copyID, List<BorrowHistoryData> borrowHistory, String loanDuration) {
 		this.id = copyID;
 		this.borrowHistory = borrowHistory;
 		this.loanDuration = loanDuration;
-		
 		this.currentInfo = new BorrowHistoryData();
 	}
 
@@ -23,11 +24,11 @@ public class CopyData {
 		this.id = id;
 	}
 
-	public BorrowHistoryData[] getBorrowHistory() {
+	public List<BorrowHistoryData> getBorrowHistory() {
 		return borrowHistory;
 	}
 
-	public void setBorrowHistory(BorrowHistoryData[] borrowHistory) {
+	public void setBorrowHistory(List<BorrowHistoryData> borrowHistory) {
 		this.borrowHistory = borrowHistory;
 	}
 
