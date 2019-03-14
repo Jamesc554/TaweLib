8bd31719dbfff569b26ad64ace313fe413934cd8
Kwaherif <45173966+Kwaherif@users.noreply.github.com>
2019-03-14 20:24:35 +0000

    Add files via upload

---
+++ src/screen/EventScreen.java
@@ -0,0 +1,126 @@
+package screen;
+
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import library.Library;
+import library.LibraryResources;
+import resources.Book;
+import resources.CopyData;
+import resources.DVD;
+import resources.Laptop;
+import resources.Resource;
+import resources.VideoGame;
+import user.User;
+
+/**
+ * <h1>Event Screen.</h1>
+ * <p>
+ * This class represents the event screen. This is the screen seen after
+ * pressing the events button.
+ * </p>
+ * 
+ * @author Deyan Naydenov
+ * @version 1.0
+ */
+public class EventScreen extends Screen implements Initializable {
+	// TOP TOOL BAR - COMMON BETWEEN SCREENS - COPY FROM HERE - MAKE SURE THE IDs IN
+	// SCENEBUILDER ARE OF THE SAME NAME AS THE VARIBLES HERE!!!!!
+	@FXML
+	private Text fineText;
+
+	@FXML
+	private ListView events;
+	
+	@FXML
+	private ListView pastEvents;
+	
+	@FXML
+	private TableColumn<EventTableData, String> eventID;
+	
+	@FXML
+	private TableColumn<EventTableData, String> title;
+	
+	@FXML
+	private TableColumn<EventTableData, String> date;
+
+	@FXML
+	private TableColumn<EventTableData, String> time;
+	@FXML
+	private TableColumn<EventTableData, String> description;
+	
+	
+	
+	@FXML
+	private TableColumn<EventTableData, String> eventAttendees;
+
+	public void start() {
+		Pane root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("fxml/home.fxml"));
+			ScreenManager.setCurrentScene(new Scene(root, 1280, 720));
+			// setupEvents();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	 */
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File(Library.getCurrentLoggedInUser().getProfImage()));
+		} catch (IOException e) {
+
+		}
+
+		eventID.setCellValueFactory(new PropertyValueFactory<EventTableData, String>("eventID"));
+		title.setCellValueFactory(new PropertyValueFactory<EventTableData, String>("eventName"));
+		date.setCellValueFactory(new PropertyValueFactory<EventTableData, String>("eventDate"));
+		time.setCellValueFactory(new PropertyValueFactory<EventTableData, String>("eventEnd"));
+		eventAttendees.setCellValueFactory(new PropertyValueFactory<EventTableData, String>("eventAttendees"));
+
+		if (Library.currentUserIsLibrarian()) {
+			issueDeskBtn.setVisible(true);
+		}
+		
+
+		User loggedInUser = Library.getCurrentLoggedInUser();
+
+		userIcon.setImage(SwingFXUtils.toFXImage(img, null));
+		}
+
+		
+	
+
+
+	public void onEnter(ActionEvent actionEvent) {
+		Library.setSearchStringText(searchBar.getText());
+		ScreenManager.changeScreen(new SearchResultScreen());
+	}
+}
---
+++ src/screen/EventTableData.java
@@ -0,0 +1,72 @@
+package screen;
+import javafx.beans.property.SimpleStringProperty;
+
+
+public class EventTableData {
+	private final SimpleStringProperty eventID;
+	private final SimpleStringProperty title;
+	private final SimpleStringProperty date;
+	private final SimpleStringProperty time;
+	private final SimpleStringProperty eventAttendees;
+	private final SimpleStringProperty description;
+	
+
+	public EventTableData(String eventID, String title, String date,
+			String time, String eventAttendees, String description) {
+		
+		this.eventID = new SimpleStringProperty(eventID);
+		this.title = new SimpleStringProperty(title);
+		this.date = new SimpleStringProperty(date);
+		this.time = new SimpleStringProperty(time);
+		this.eventAttendees = new SimpleStringProperty(eventAttendees);
+		this.description = new SimpleStringProperty(description);
+	}
+
+	/**
+	 * Gets the resource's ID.
+	 * @return resourceID
+	 * The resource's ID.
+	 */
+	public String getEventID() {
+		return eventID.get();
+	}
+	
+	/**
+	 * Gets the resource's Title.
+	 * @return resourceTitle
+	 * The resource's Title.
+	 */
+	public String getTitle() {
+		return title.get();
+	}
+	
+	/**
+	 * Gets the date the copy of the resource was borrowed on.
+	 * @return borrowDate
+	 * The date the resource was borrowed.
+	 */
+	public String getDate() {
+		return date.get();
+	}
+	
+	/**
+	 * Gets the date the copy of the resources was returned on.
+	 * @return returnDate
+	 * The date the copy was returned.
+	 */
+	public String getTime() {
+		return time.get();
+	}
+	
+	/**
+	 * Gets whether the copy is overdue.
+	 * @return overdue
+	 * Whether the copy is overdue (returned after the returnDate).
+	 */
+	public String description() {
+		return description.get();
+	}
+	public String eventAttendees() {
+		return eventAttendees.get();
+	}
+}
