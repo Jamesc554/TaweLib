e59c904ff5674d96e868fbbbf40b92e331a627f4
Kwaherif <45173966+Kwaherif@users.noreply.github.com>
2019-03-14 20:25:02 +0000

    Add files via upload

--- src/event/Event.java
+++ src/event/Event.java
@@ -3,12 +3,12 @@ package event;
 import java.util.ArrayList;
 
 public class Event {
-
+	
+	private String eventID;
     private String title;
     private String date;
     private String time;
-    private int maxNumberOfAttending;
-    private int currentNumberOfAttending;
+    private int eventAttendees;
     private String description;
 
     public Event(String title, String date, String time, int maxNumberOfAttending, int currentNumberOfAttending,
@@ -16,9 +16,20 @@ public class Event {
         this.title = title;
         this.date = date;
         this.time = time;
+        /*
         this.maxNumberOfAttending = maxNumberOfAttending;
         this.currentNumberOfAttending = currentNumberOfAttending;
+        */
         this.description = description;
+        this.eventAttendees = eventAttendees;
+    }
+   //Placeholder
+    public String getEventID() {
+    	return eventID;
+    }
+    //Placeholder
+    public void setEventID(String eventID) {
+    	this.eventID = eventID;
     }
 
     public String getDate() {
@@ -44,7 +55,7 @@ public class Event {
     public void setTime(String time) {
         this.time = time;
     }
-
+    /*
     public int getMaxNumberOfAttending() {
         return maxNumberOfAttending;
     }
@@ -60,7 +71,14 @@ public class Event {
     public void setCurrentNumberOfAttending(int currentNumberOfAttending) {
         this.currentNumberOfAttending = currentNumberOfAttending;
     }
+	*/
+    public int getEventAttendees() {
+        return eventAttendees;
+    }
 
+    public void setEventAttendees(int eventAttendees) {
+        this.eventAttendees = eventAttendees;
+    }
     public String getDescription() {
         return description;
     }
